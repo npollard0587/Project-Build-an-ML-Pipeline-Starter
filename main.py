12554f1fac4dc483cfa1cbecc7045c4b9e99cb92
Nicole Pollard <nicole0587@gmail.com>
2026-06-21 22:52:41 +0000

    Implement data validation tests

--- main.py
+++ main.py
@@ -67,9 +67,19 @@ def go(config: DictConfig):
             pass
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
+            
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                "main",
+                env_manager="conda",
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+                },
+            )
             pass
 
         if "data_split" in active_steps:
